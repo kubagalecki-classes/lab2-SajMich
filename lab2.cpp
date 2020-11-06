df573cf963310687d9b9e1c2a2a09cf8ba72cc45
SajMich <michal.sajewicz.stud@pw.edu.pl>
2020-11-06 12:17:15 +0000

    solved

--- lab2.cpp
+++ lab2.cpp
@@ -1,6 +1,141 @@
 #include <iostream>
 
+class Wektor
+{
+public:
+    // konstruktor parametryczny
+    Wektor(int n) : dlugosc(n), pojemnosc(n)
+    {
+        w = new double[dlugosc];
+        for (int i = 0; i < dlugosc; i++) {
+            w[i] = 0;
+        }
+        std::cout << "konstruktor parametryczny"
+                  << "\n";
+    }
+
+    // konstruktor kopiujacy
+    Wektor(const Wektor& W) : dlugosc(W.dlugosc), pojemnosc(W.pojemnosc)
+    {
+        w = new double[dlugosc];
+        w = W.w;
+        std::cout << "Konstruktor kopiujacy"
+                  << "\n";
+    }
+
+    // konstruktor przenoszacy
+    Wektor(Wektor&& W) : w(nullptr), dlugosc(0), pojemnosc(0)
+    {
+        w           = W.w;
+        dlugosc     = W.dlugosc;
+        pojemnosc   = W.pojemnosc;
+        W.w         = nullptr;
+        W.dlugosc   = 0;
+        W.pojemnosc = 0;
+        std::cout << "Konstruktotr przenoszacy"
+                  << "\n";
+    }
+
+    // destruktor
+    ~Wektor()
+    {
+        delete[] w;
+        std::cout << "destruktor"
+                  << "\n";
+    }
+
+    int get() { return dlugosc; };
+
+    void print()
+    {
+        std::cout << "Wektor: "
+                  << "\n";
+        for (int i = 0; i < dlugosc; i++) {
+            std::cout << w[i] << "\n";
+        }
+    }
+
+    // zmiana dlugosci
+    void zmienDlugosc(int m)
+    {
+        if (m <= pojemnosc) {
+            dlugosc = m;
+            for (int i = m; i < pojemnosc; i++) {
+                w[i] = 0;
+            }
+        }
+
+        else {
+            double* tmp = new double[m];
+            for (int i = 0; i < dlugosc; i++) {
+                tmp[i] = w[i];
+            }
+            for (int i = dlugosc; i < m; i++) {
+                tmp[i] = 0;
+            }
+            delete[] w;
+            w         = tmp;
+            dlugosc   = m;
+            pojemnosc = m;
+        }
+    }
+
+    Wektor pow2(Wektor&& arg)
+    {
+        Wektor ret_val{std::move(arg)};
+        for (int i = 0; i < dlugosc; i++) {
+            std::cout << i << "\n";
+        }
+        return ret_val;
+    }
+
+    // przeciazanie operatorow
+    double& operator[](int);
+    Wektor& operator=(const Wektor&);
+    Wektor& operator=(Wektor&&);
+
+private:
+    int     dlugosc;
+    int     pojemnosc;
+    double* w;
+};
+
+// przeciazanie operatora []
+double& Wektor::operator[](int index)
+{
+    if (index >= dlugosc) {
+        zmienDlugosc(index);
+    }
+    return w[index];
+}
+// kopiujacy operator przypisania =
+Wektor& Wektor::operator=(const Wektor& W)
+{
+    if (this != &W) {
+        delete w;
+        dlugosc   = W.dlugosc;
+        pojemnosc = W.pojemnosc;
+        w         = new double[dlugosc];
+        for (int i = 0; i < W.dlugosc; i++) {
+            w[i] = W.w[i];
+        }
+    }
+    return *this;
+}
+
+// przenoszacy opertor przypisania
+Wektor& Wektor::operator=(Wektor&& W)
+{
+    w         = std::move(W.w);
+    dlugosc   = std::move(W.dlugosc);
+    pojemnosc = std::move(W.pojemnosc);
+}
+
 int main()
 {
-    puts("Hello, World!");
+    Wektor v1{2};
+    v1[0] = 10;
+    v1[1] = 6;
+    v1.print();
+    std::cout << "liczba elementow: " << v1.get() << "\n";
 }
--- zal/ResourceManager.hpp
+++ zal/ResourceManager.hpp
@@ -4,5 +4,55 @@
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+  public:
+    Resource R;
+    double get(){return R.get()};
+
+
+    //Konstruktor kopiujacy
+    ResourceManager(const ResourceManager& RM)
+    {
+        R = new Resource;
+        R = RM.R;
+    }
+    //kopiujacy opertor przypisania
+
+    ResourceManager& operator=(const ResourceManager&);
+
+    //konstruktor przenoszacy
+    ResourceManager(ResourceManager&& RM)
+    {
+        R           = RM.R;
+        RM.R         = nullptr;
+    }
+
+    //przenoszacy operator przypisania
+    ResourceManager& operator=(ResourceManager&&);
+    
+    //destruktor
+    ~ResourceManager()
+    {
+        delete[] R;
+    }
 };
+
+//kopiujacy operator przypisania
+ResourceManager& ResourceManager::operator=(const ResourceManager& RM)
+{
+    if (this != &RM) {
+        delete R;
+
+        R         = new Resource;
+        R=RM;
+
+    }
+    return *this;
+}
+
+
+//przenoszacy operator przypisania
+ResourceManager& ResourceManager::operator=(ResourceManager&& RM)
+{
+    R         = std::move(RM.R);
+}
+
